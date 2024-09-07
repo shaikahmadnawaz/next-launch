6df768fa560a98f0406ffc9347ea7eb8740ca201
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2024-09-07 18:48:17 +0530

    feat(db): postgresql db setup using drizzle and supabase

---
+++ db/schema/users.ts
@@ -0,0 +1,12 @@
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export type InsertUser = typeof users.$inferInsert;
+export type SelectUser = typeof users.$inferSelect;
---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({
+  path: ".env.local",
+});
+
+export default defineConfig({
+  schema: "./db/schema/*.ts",
+  out: "./migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+});
